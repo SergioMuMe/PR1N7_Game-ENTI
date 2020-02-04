536d75398cad2216843f8e163742c26e344bd8e8
MarcCR <34023788+MarcCR@users.noreply.github.com>
2020-02-04 20:37:58 +0100

    multiple point movement platform
    codigo pulido + comentarios

--- Assets/Scripts/movePlatformV2.cs
+++ Assets/Scripts/movePlatformV2.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class movePlatformV2 : MonoBehaviour
 {
-    
+    //controla la velocidad a la que se mueve la plataforma entre punto y punto (si le damos un valores altos [por encima de 1] puede que de problemas)
     public float speed;
     private Rigidbody2D rb;
     private bool going = true;
     private float fixedDelta;
     private float timePlatform;
+
+    //aun no entiendo 100% como va, pero con delay 1, las plataformas no dejan de moverse, con delay > 1 se paran durante un tiempo cada vez que llegan a un punto de la array
+    public float delay = 2;
+    //durante la ejecucion la I indica la ultima posicion de la array por el que se ha pasado
     public int i = 0;
     public Vector2[] pos;
-    public int posSize;
 
     void Start()
     {
+        //nos aseguramos de que el rigidbody sea kinematic
         rb = GetComponent<Rigidbody2D>();
         rb.isKinematic = true;
     }
@@ -32,24 +36,24 @@ public class movePlatformV2 : MonoBehaviour
             {
                 movePlatform(pos[i], pos[i + 1], timePlatform);
             }
-            else if (timePlatform >= 2)
+            else if (timePlatform >= delay)
             {
                 timePlatform = 0;
                 i++;               
             }
-            if (i >= posSize - 1)
+            if (i >= pos.Length - 1)
             {
                 going = false;
             }
 
         }
-        if (!going)
+        else if (!going)
         {
             if (timePlatform <= 1)
             {
                 movePlatform(pos[i], pos[i-1], timePlatform);
             }
-            else if (timePlatform >= 2)
+            else if (timePlatform >= delay)
             {
                 timePlatform = 0;
                 i--;
