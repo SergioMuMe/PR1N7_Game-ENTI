8de2ef10ca73c075377f8d517bad78a05f70e058
MarcCR <34023788+MarcCR@users.noreply.github.com>
2020-02-04 14:22:31 +0100

    bouncer0.1

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -4,37 +4,20 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    private CharacterController_Roger playerScript;
-    private Collider2D cl;
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerScript = GetComponent<CharacterController_Roger>();
-        cl = GetComponent<Collider2D>();
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        OnTriggerEnter2D(cl);
-        OnTriggerExit2D(cl);
-    }
-
-    private void OnTriggerEnter2D(Collider2D Player)
-    {
-        if(Player.tag != "Player")
+        if(other.tag == "Player")
         {
-            return;
+            other.gameObject.GetComponent<CharacterController_Roger>().jumpForce *= 2;
         }
-        playerScript.jumpForce = playerScript.jumpForce * 2;
     }
 
-    private void OnTriggerExit2D(Collider2D Player)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if (Player.tag != "Player")
+        if (other.tag == "Player")
         {
-            return;
+            other.gameObject.GetComponent<CharacterController_Roger>().jumpForce /= 2;
         }
-        playerScript.jumpForce = playerScript.jumpForce/2;
     }
 }
