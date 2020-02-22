1c840813e926e3a3d48199607163a34b1cbb4c50
Sergio Murillo Mendez <sergio.murillo.mendez@gmail.com>
2020-02-23 00:17:03 +0100

    Prefab Placeholder Pila + Arreglado algun detalle de nivel
    He tocado algunos niveles para poder hacer una build a la que poder enseñar a unos amigos.

---
+++ Assets/Scripts/Elementos/Pila.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pila : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.2f, 0.5f, 0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("CollisionPlayer");
+            Destroy(gameObject);
+        }
+    }
+}
