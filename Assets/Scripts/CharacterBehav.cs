8579382c3ed1687cc62704cff0275c12271405c5
Gerchis <rogerba97@gmail.com>
2020-02-24 16:30:52 +0100

    CloneAnimation

--- Assets/Scripts/CharacterBehav.cs
+++ Assets/Scripts/CharacterBehav.cs
@@ -71,6 +71,9 @@ public class CharacterBehav : MonoBehaviour
     //Color de las particulas
     public ParticleSystemRenderer partRender;
 
+    public ParticleSystem nanoStart;
+    public ParticleSystem nanoEnd;
+
     //Almacena la distancia a la que detecta el suelo
     public float groundedPrecision = 0.0f;
     //Almacena la distancia al suelo
@@ -107,16 +110,16 @@ public class CharacterBehav : MonoBehaviour
     //Almacena el multiplicador que acelera el tiempo
     public float timeAcceleration = 0.0f;
 
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
+        anim = GetComponent<Animator>();
+
         groundDistance = GetComponent<Collider2D>().bounds.extents.y;
         frontDistance = GetComponent<Collider2D>().bounds.extents.x;
-
-        part = GetComponentInChildren<ParticleSystem>();
-        partRender = GetComponentInChildren<ParticleSystemRenderer>();
 
         maxHeight = maxHeightDefault;
         jumpForce = jumpForceDefault;
@@ -125,7 +128,7 @@ public class CharacterBehav : MonoBehaviour
         {
             initPos = transform.position;
 
-            initInputTime = Time.time;
+            initInputTime = Time.time * 1000;
         }
     }
 
@@ -250,7 +253,7 @@ public class CharacterBehav : MonoBehaviour
                     isInteracting = false;
                 }
                 // Esperar a que la lista tenga "END"
-                if (inputs[iteration].time <= (Time.time * 1000) - initInputTime)
+                if (iteration >= 0 && iteration < inputs.Capacity && inputs[iteration].time <= (Time.time * 1000) - initInputTime)
                 {
 
                     switch (inputs[iteration].ci)
@@ -280,9 +283,8 @@ public class CharacterBehav : MonoBehaviour
                             break;
                         case CommandsInputsEnum.END:
                             direction = DirectionInputs.NONE;
-                            initInputTime = (Time.time * 1000);
+                            anim.SetTrigger("End");
                             iteration = -1;
-                            transform.position = initPos;
                             break;
                         default:
                             break;
@@ -358,7 +360,7 @@ public class CharacterBehav : MonoBehaviour
             case DirectionInputs.NONE:
                 break;
             case DirectionInputs.RIGHT:
-                if (!isJumping && !hitRight || !isJumping && hitLeft.collider.tag == "Clone")
+                if (!isJumping && !hitRight || !isJumping && hitRight.collider.tag == "Clone")
                 {
                     rb.AddForce(Vector2.right * baseSpeed, ForceMode2D.Force);
                 }
@@ -380,6 +382,19 @@ public class CharacterBehav : MonoBehaviour
             default:
                 break;
         }
+    }
+
+    public void EndIterationParticles()
+    {
+        nanoEnd.Play();
+    }
+
+    public void StartIterationParticles()
+    {
+        initInputTime = (Time.time * 1000);
+        iteration = 0;
+        transform.position = initPos;
+        nanoStart.Play();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
