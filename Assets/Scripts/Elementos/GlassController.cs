33e5783962540413c6e06bb274af0ba57ec9ff13
Sergio Murillo Mendez <sergio.murillo.mendez@gmail.com>
2020-05-12 20:03:05 +0200

    commit

---
+++ Assets/Scripts/Elementos/GlassController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassController : MonoBehaviour
+{
+
+    MeshRenderer myMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMesh = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.Instance.isGamePaused)
+        {
+            myMesh.enabled = false;
+        } else
+        {
+            myMesh.enabled = true;
+        }
+    }
+}
