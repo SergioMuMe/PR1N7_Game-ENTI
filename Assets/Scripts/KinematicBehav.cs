f224e18bc31af2b059a30dd177583eb64d73f699
Gerchis <56065317+Gerchis@users.noreply.github.com>
2020-05-13 13:43:45 +0200

    KinematicBuilds

---
+++ Assets/Scripts/KinematicBehav.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicBehav : MonoBehaviour
+{
+    public Camera kinCam;
+    public Camera cam;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            kinCam.rect = cam.rect;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            kinCam.gameObject.SetActive(false);
+        }
+    }
+}
